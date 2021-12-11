1550fdbd095fde97904a856e3c444492296e10a8
JoshuaMcroberts <josh.mcr@hotmail.co.uk>
2021-12-11 20:27:33 +0000

    Added logo colours for couriers list

--- start.py
+++ start.py
@@ -1,11 +1,12 @@
 from libraries import *
 from text import *
 
+# CHARACTER CLASS
 class character:
     def __init__(self, play_name, char_name, num_plate, gen):
-        self.player_name = play_name
-        self.character_name = char_name
-        self.number_plate = num_plate
+        self.player_name = pr_colour("l_blue", play_name)
+        self.character_name = pr_colour( "l_blue" , char_name )
+        self.number_plate =  pr_colour("num_p", num_plate)
         self.inventory = []
         self.gender = gen
         
@@ -18,30 +19,33 @@ class character:
             
         con_cat = play_name + char_name + num_plate + gen
         
-        cur_op = len(con_cat)%8
+        cur_op = len(con_cat)%4
         
-        # unself this.
-        cur_list = [pr_colour(1, "Parcel Force"), pr_colour(4, "FedEx"), pr_colour(2, "Amazon Prime Delivery"), pr_colour(3, "UPS"), 
-                         pr_colour(1, "Parcel Force"), pr_colour(4, "FedEx"), pr_colour(2, "Amazon Prime Delivery"), pr_colour(3, "UPS") ]
+        amazon = pr_colour("amazon", " A") + pr_colour("prime","maz")+ pr_colour("amazon","on Prime Delivery ") 
+        fedex = pr_colour("fed", " Fed") + pr_colour("ex","Ex ")
+        p_force = pr_colour("p_force", " Parcel Force ")
+        ups = pr_colour("ups", " UPS ")
+        
+        cur_list = [amazon, fedex, ups, p_force]
         
         self.courier = cur_list[cur_op]
         
         
     def set_char_name(self, c_name):
-        self.character_name = c_name
+        self.character_name = pr_colour( "l_blue" , c_name )
 
     def get_char_name(self):
         return self.character_name
 
     def set_player_name(self, p_name):
-        self.player_name = p_name
+        self.player_name = pr_colour( "l_blue",p_name)
 
     def get_player_name(self):
         return self.player_name
 
     def set_num_plate(self, num_plate):
-        self.number_plate = num_plate
-
+        self.number_plate = pr_colour("num_p", num_plate)
+        
     def get_num_plate(self):
         return self.number_plate
     
@@ -82,15 +86,20 @@ class character:
         print_tab("Gender: {}\n\tPronoun 1: {}\n\tPronoun 2: {}\n".format(self.gender, self.pronoun1, self.pronoun2))
         self.display_inventory()
 
+
+# CLEANS TEXT TO SIMPLE FORM
 def san_text(text):
     text = text.lower()
     text = text.strip()
     return text
 
+
+# ENTER PLAYER NAME - CHECK FOR NUMBER PLATE PLAYER
 def enter_name():
+    
     clear_screen()
-    tup_list = [("daniel","PLATE1"),("rebecca","PLATE2"),("andrew","PLATE3"),("joel","PLATE4"),("nathanael","PLATE5")]
-    en_name = input("\tPlease enter your "+ pr_colour( 3,"First Name") +": ")
+    tup_list = [("daniel"," PLATE1 "),("rebecca"," PLATE2 "),("andrew"," PLATE3 "),("joel"," PLATE4 "),("nathanael"," PLATE5 ")]
+    en_name = input("\tPlease enter your "+ pr_colour( "l_yellow","First Name") +": ")
     plate_name = san_text(en_name)
     
     num_plate = 0
@@ -99,21 +108,25 @@ def enter_name():
             num_plate = num
 
     if num_plate == 0:
-        num_plate = "REG MISSING"
+        num_plate = " REG MISSING "
 
     return en_name, num_plate
-    
+
+
+# ENTER CHARACTER NAME    
 def enter_character_name():
     clear_screen()
-    character_name = input("\tPlease enter your "+ pr_colour( 3,"Characters name") +": ")
+    character_name = input("\tPlease enter your "+ pr_colour( "l_yellow","Characters name") +": ")
     return character_name
 
+
+# ENTER CHARACTER GENDER
 def enter_char_gender():
     
     valid = False
     while not valid:
         clear_screen()
-        gen = input("\tIs your Character {} or {}?: ".format(pr_colour(3, "MALE"), pr_colour(3,"FEMALE")))
+        gen = input("\tIs your Character {} or {}?: ".format(pr_colour("l_yellow", "male"), pr_colour("l_yellow","female")))
         gen = san_text(gen)
         
         if gen == "male" or gen == "female":
@@ -121,42 +134,46 @@ def enter_char_gender():
         else:
             print_tab("\n\tType MALE or FEMALE")
             pause()
-    
     return gen
      
-
+     
+# DISPLAY HELP TEXT
 def help_text():
     clear_screen()
     print_tab("Help text will go here!")
 
 
+# DISPLAY ABOUT TEXT
 def about_text():
     clear_screen()
     print_tab("About text will go here!")
 
+
+# DISPLAY ASCII ART
 def game_intro():
     print_tab("Delviery Dilemma\n")
 
+
+# GAME FUNCTION
 def game():
     clear_screen()
-    # print_tab("Character Set Up\n")
-    # pause()
     p_name, num_p = enter_name()
     c_name = enter_character_name()
     c_gen = enter_char_gender()
 
     pc = character(p_name, c_name, num_p, c_gen)
-    # pc.print_all()
-    # pause()
-    pause()
-    
+ 
+
+    # uncomment
     char_name = pc.get_char_name()
     pro_1, pro_2 = pc.get_pronouns()
     cur = pc.get_courier()
     
-    # Game Intro
+    # Game ACT 1
     act_1_intro(char_name, pro_1, pro_2, cur)
 
+
+# MAIN FUNCTION
 def main():
 
     game_intro()
@@ -183,7 +200,7 @@ def main():
             about_text()
             pause()
         elif main_op == 4:
-            print_tab(pr_colour(4,"Bye Bye"))
+            print_tab(pr_colour("purple","Bye Bye"))
            
             ext = True
         else:
@@ -193,4 +210,4 @@ def main():
   
 if __name__ == "__main__":
     main()
-    # game()
+    
