d74cd4cbcd02b8b05e8e4d444bd1194aca51476f
JoshuaMcroberts <josh.mcr@hotmail.co.uk>
2021-12-12 12:39:33 +0000

    added final 5 verse, added 3rd pronoun

--- text.py
+++ text.py
@@ -1,7 +1,7 @@
 from libraries import *
 # from start import character
 
-def act_1_intro(chac_name, pronoun1, pronoun2, courier):
+def act_1_intro(chac_name, pronoun1, pronoun2, pronoun3, courier):
       
     clear_screen()
     print_tab("It was the night before the delivery deadline before Christmas and all through the house,")
@@ -11,10 +11,10 @@ def act_1_intro(chac_name, pronoun1, pronoun2, courier):
     pause()
 
     clear_screen()
-    print_tab("The rush was on for the Christmas deadline you see")
+    print_tab("The rush was on for the Christmas deadline, you see")
     print_tab("and " + chac_name + " had left the parcel packing until after " + pronoun1 + " tea.")
     print_tab("Each box for a friend would be stacked by the stair,")
-    print_tab("In hopes that in the morning" + courier + "would be there.")
+    print_tab("In hopes that in the morning " + courier + " would be there.")
     pause()
     
     clear_screen()
@@ -25,8 +25,8 @@ def act_1_intro(chac_name, pronoun1, pronoun2, courier):
     pause()
 
     clear_screen()
-    print_tab("With the parcel signed for, and courier on his way,")
-    print_tab(chac_name+ " went back to " + pronoun1 + " busy wrapping day.")
+    print_tab("With the parcel signed for, and Courier on his way,")
+    print_tab(chac_name + " went back to " + pronoun1 + " very busy wrapping day.")
     print_tab("With the new delivery almost opened another distraction was at hand,")
     print_tab("From the living room a phone rang, completely unplanned. ")
     pause()
@@ -42,7 +42,7 @@ def act_1_intro(chac_name, pronoun1, pronoun2, courier):
     print_tab("Wrenched from a dream as a good bit drew near,")
     print_tab("By a rap on a door? Ah the Courier is here! ")
     print_tab("A swift hustle and bustle of parcels to van,")
-    print_tab("and a thank you mince pie for the" + courier + ", delivery man.")
+    print_tab("and a thank you mince pie for the " + courier + ", delivery man.")
     pause()
 
     clear_screen()
@@ -63,14 +63,44 @@ def act_1_intro(chac_name, pronoun1, pronoun2, courier):
     clear_screen()
     print_tab("“That’s Christmas Ruined! I must get it on track!")
     print_tab("I must find a way to get that parcel back!”")
-    print_tab("With fleetest of fingers and no delay,")
-    print_tab("On to the" + courier + "website to try save the day!")
+    print_tab("With fleetest of fingers and no time for delay,")
+    print_tab("On to the " + courier + " website to try save the day!")
     pause()
 
-    
+    clear_screen()
+    print_tab("A scuffle of papers on desk had unveiled,")
+    print_tab("the tracking information of the gift wrongly mailed.")
+    print_tab("On the " + courier + " website there was a pop-up you see,")
+    print_tab("and with a click of ‘reject all cookies’ " + chac_name + " thought “They’re not tracking me!”")
+    pause()
 
+    clear_screen()
+    print_tab("With a quick search in the parcel tracking bar,")
+    print_tab("an HTTP request was sent over the wire.")
+    print_tab("Through to server, and thence a database,")
+    print_tab("with information retrieved, the path was retraced. ")
+    pause()
 
+    clear_screen()
+    print_tab("It popped up on the screen, the information was clear,")
+    print_tab("The delivery van was headed to West Devonshire.")
+    print_tab("As a local of sunny Weston-on-Trent, ")
+    print_tab(chac_name + " thought it was a strange place for the parcel to be sent.")
+    pause()
 
+    clear_screen()
+    print_tab("With no time left for a return and resend,")
+    print_tab(chac_name + " found " + pronoun3 + "self at " + pronoun1 + " wits end!")
+    print_tab("And then in a split second, an idea did appear,")
+    print_tab("“I’ll go there, switch the parcel, then be in the clear!”")
+    pause()
+
+    clear_screen()
+    print_tab("A Delivery Dilemma so hard to compound, ")
+    print_tab("would require " + pronoun3 + " to put " + pronoun1 + " boots on the ground.")
+    print_tab("With the solution to the problem not an easy one,")
+    print_tab(chac_name + "’s Christmas Adventure had just begun!")
+    pause()
 
 if __name__ == "__main__":
-    act_1_intro("Bob", "his", "he", " UPS ")
+    act_1_intro("Bob", "his", "he", "him", " UPS ")
