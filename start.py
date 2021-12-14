f501d1319db14b70df0aa629f983c50c646e4fde
JoshuaMcroberts <josh.mcr@hotmail.co.uk>
2021-12-14 22:11:19 +0000

    created game class and restructured

--- start.py
+++ start.py
@@ -1,13 +1,160 @@
 from libraries import *
 from text import *
 
+# GAME CLASS
+class N_game:
+    def __init__(self):
+        self.player_name = ""
+        self.number_plate = ""
+        self.pc = ""
+        
+    def set_player_name(self, p_name):
+        self.player_name = pr_colour( "l_blue",p_name)
+    
+    def get_player_name(self):
+        return self.player_name
+
+    def set_num_plate(self, num_plate):
+        self.number_plate = pr_colour("num_p", num_plate)
+        
+    def get_num_plate(self):
+        return self.number_plate    
+    
+    def get_character(self):
+        return self.pc
+    
+    def set_courier(self):
+        # Courier Picker
+        con_cat = len(self.player_name)*3
+        cur_op = con_cat%4
+        
+        amazon = pr_colour("amazon", " A") + pr_colour("prime","maz")+ pr_colour("amazon","on Prime Delivery ") 
+        fedex = pr_colour("fed", " Fed") + pr_colour("ex","Ex ")
+        p_force = pr_colour("p_force", " Parcel Force ")
+        ups = pr_colour("ups", " UPS ")
+        
+        cur_list = [amazon, fedex, ups, p_force]
+        self.courier = cur_list[cur_op]
+        # End Courier Picker
+        
+    def get_courier(self):
+        return self.courier
+       
+    # ENTER PLAYER NAME - CHECK FOR NUMBER PLATE PLAYER
+    def enter_name(self):
+        
+        tup_list = [("daniel"," PLATE1 "),("rebecca"," PLATE2 "),("andrew"," PLATE3 "),("joel"," PLATE4 "),("nathanael"," PLATE5 ")]
+        
+        correct = False
+        while not correct:
+            clear_screen()
+            en_name = input("\tPlease enter your "+ pr_colour( "l_yellow","First Name") +": ")
+            en_name = pr_colour("l_blue", en_name)
+            
+            
+            val2 = False
+            while not val2:
+                clear_screen()
+                print_tab("You have entered: " + en_name)
+                val = input("\tIs this correct? (Y/N): ")
+                
+                if val == "Y":
+                    correct = True
+                    val2 = True
+                elif val == "N":
+                    correct = False
+                    val2 = True
+                else:
+                    print()
+                    print_tab("Enter Y for Yes or N for No")
+                    pause()
+        
+        
+        plate_name = san_text(en_name)
+        
+        num_plate = 0
+        for name, num in tup_list:
+            if plate_name == name:
+                num_plate = num
+
+        if num_plate == 0:
+            num_plate = " REG MISSING "
+
+        self.set_player_name(en_name)
+        self.set_num_plate(num_plate)
+    
+    
+    def create_char(self):
+        name = self.enter_character_name()
+        gen = self.enter_char_gender()
+        self.pc = Character()
+        self.pc.set_char_name(name)
+        self.pc.set_pronouns(gen)
+        
+        
+    # ENTER CHARACTER NAME    
+    def enter_character_name(self):
+        
+        correct = False
+        while not correct:
+            clear_screen()
+            character_name = input("\tPlease enter your "+ pr_colour( "l_yellow","Characters name") +": ")
+            character_name = pr_colour("l_blue", character_name)
+            
+            
+            val2 = False
+            while not val2:
+                clear_screen()
+                print_tab("You have entered: " + character_name)
+                val = input("\tIs this correct? (Y/N): ")
+                
+                if val == "Y":
+                    correct = True
+                    val2 = True
+                elif val == "N":
+                    correct = False
+                    val2 = True
+                else:
+                    print()
+                    print_tab("Enter Y for Yes or N for No")
+                    pause()
+        
+        
+        return character_name
+
+
+    # ENTER CHARACTER GENDER
+    def enter_char_gender(self):
+        
+        valid = False
+        while not valid:
+            clear_screen()
+            gen = input("\tIs your Character {} or {}?: ".format(pr_colour("l_yellow", "male"), pr_colour("l_yellow","female")))
+            gen = san_text(gen)
+            
+            if gen == "male" or gen == "female":
+                valid = True
+            else:
+                print_tab("\n\tType MALE or FEMALE")
+                pause()
+        return gen
+    
+
 # CHARACTER CLASS
-class character:
-    def __init__(self, play_name, char_name, num_plate, gen):
-        self.player_name = pr_colour("l_blue", play_name)
-        self.character_name = pr_colour( "l_blue" , char_name )
-        self.number_plate =  pr_colour("num_p", num_plate)
+class Character:
+    def __init__(self):
         self.inventory = []
+        self.gender = ""
+        self.character_name = ""
+        
+        
+    def set_char_name(self, c_name):
+        self.character_name = pr_colour( "l_blue" , c_name )
+
+    def get_char_name(self):
+        return self.character_name
+    
+    def set_pronouns(self, gen):
         self.gender = gen
         
         if gen == "male":
@@ -18,44 +165,9 @@ class character:
             self.pronoun1 = "her"
             self.pronoun2 = "she"
             self.pronoun3 = "her"
-            
-        con_cat = play_name + char_name + num_plate + gen
-        
-        cur_op = len(con_cat)%4
-        
-        amazon = pr_colour("amazon", " A") + pr_colour("prime","maz")+ pr_colour("amazon","on Prime Delivery ") 
-        fedex = pr_colour("fed", " Fed") + pr_colour("ex","Ex ")
-        p_force = pr_colour("p_force", " Parcel Force ")
-        ups = pr_colour("ups", " UPS ")
-        
-        cur_list = [amazon, fedex, ups, p_force]
-        
-        self.courier = cur_list[cur_op]
-        
-        
-    def set_char_name(self, c_name):
-        self.character_name = pr_colour( "l_blue" , c_name )
-
-    def get_char_name(self):
-        return self.character_name
-
-    def set_player_name(self, p_name):
-        self.player_name = pr_colour( "l_blue",p_name)
-
-    def get_player_name(self):
-        return self.player_name
-
-    def set_num_plate(self, num_plate):
-        self.number_plate = pr_colour("num_p", num_plate)
-        
-    def get_num_plate(self):
-        return self.number_plate
     
     def get_pronouns(self):
         return self.pronoun1, self.pronoun2, self.pronoun3
-    
-    def get_courier(self):
-        return self.courier
     
     def add_inventory(self, item):
         self.inventory.append(item)
@@ -84,104 +196,10 @@ class character:
 
     def print_all(self):
         clear_screen()
-        print_tab("\n\tPlayer Name: {}\n\tCharacter Name: {}\n\tNumber Plate: {}".format(self.player_name, self.character_name, self.number_plate))
+        print_tab("\n\tCharacter Name: {}".format(self.character_name))
         print_tab("Gender: {}\n\tPronoun 1: {}\n\tPronoun 2: {}\n".format(self.gender, self.pronoun1, self.pronoun2))
         self.display_inventory()
 
-
-
-# ENTER PLAYER NAME - CHECK FOR NUMBER PLATE PLAYER
-def enter_name():
-    
-  
-    tup_list = [("daniel"," PLATE1 "),("rebecca"," PLATE2 "),("andrew"," PLATE3 "),("joel"," PLATE4 "),("nathanael"," PLATE5 ")]
-    
-    correct = False
-    while not correct:
-        clear_screen()
-        en_name = input("\tPlease enter your "+ pr_colour( "l_yellow","First Name") +": ")
-        en_name = pr_colour("l_blue", en_name)
-        
-        
-        val2 = False
-        while not val2:
-            clear_screen()
-            print_tab("You have entered: " + en_name)
-            val = input("\tIs this correct? (Y/N): ")
-            
-            if val == "Y":
-                correct = True
-                val2 = True
-            elif val == "N":
-                correct = False
-                val2 = True
-            else:
-                print()
-                print_tab("Enter Y for Yes or N for No")
-                pause()
-    
-    
-    plate_name = san_text(en_name)
-    
-    num_plate = 0
-    for name, num in tup_list:
-        if plate_name == name:
-            num_plate = num
-
-    if num_plate == 0:
-        num_plate = " REG MISSING "
-
-    return en_name, num_plate
-
-
-# ENTER CHARACTER NAME    
-def enter_character_name():
-    
-    correct = False
-    while not correct:
-        clear_screen()
-        character_name = input("\tPlease enter your "+ pr_colour( "l_yellow","Characters name") +": ")
-        character_name = pr_colour("l_blue", character_name)
-        
-        
-        val2 = False
-        while not val2:
-            clear_screen()
-            print_tab("You have entered: " + character_name)
-            val = input("\tIs this correct? (Y/N): ")
-            
-            if val == "Y":
-                correct = True
-                val2 = True
-            elif val == "N":
-                correct = False
-                val2 = True
-            else:
-                print()
-                print_tab("Enter Y for Yes or N for No")
-                pause()
-    
-    
-    return character_name
-
-
-# ENTER CHARACTER GENDER
-def enter_char_gender():
-    
-    valid = False
-    while not valid:
-        clear_screen()
-        gen = input("\tIs your Character {} or {}?: ".format(pr_colour("l_yellow", "male"), pr_colour("l_yellow","female")))
-        gen = san_text(gen)
-        
-        if gen == "male" or gen == "female":
-            valid = True
-        else:
-            print_tab("\n\tType MALE or FEMALE")
-            pause()
-    return gen
-     
-     
 # DISPLAY HELP TEXT
 def help_text():
     clear_screen()
@@ -200,20 +218,17 @@ def game_intro():
 
 
 # GAME FUNCTION
-def game():
+def new_game():
     clear_screen()
-    p_name, num_p = enter_name()
-    c_name = enter_character_name()
-    c_gen = enter_char_gender()
-
-    pc = character(p_name, c_name, num_p, c_gen)
-    
-    char_name = pc.get_char_name()
-    pro_1, pro_2, pro_3 = pc.get_pronouns()
-    cur = pc.get_courier()
+    game = N_game()
+    game.enter_name()
+    game.set_courier()
+    game.create_char()
+    pc = game.get_character()
+    cour = game.get_courier()
     
     # Game ACT 1
-    act_1_intro(char_name, pro_1, pro_2, pro_3, cur)
+    act_1_intro(cour, pc)
     
     # Game ACT 2
     # - Getting in  
@@ -258,7 +273,7 @@ def main():
             main_op = 10   
 
         if main_op == 1:
-            game()
+            new_game()
         elif main_op == 2:
             help_text()
             pause()
