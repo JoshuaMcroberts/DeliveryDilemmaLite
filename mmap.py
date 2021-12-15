05e7af3004e68202f579b89b379ee9aa8df29109
JoshuaMcroberts <josh.mcr@hotmail.co.uk>
2021-12-15 22:45:11 +0000

    Map update postion, print size increase

--- mmap.py
+++ mmap.py
@@ -1,3 +1,5 @@
+from libraries import *
+
 class mind_map:
     
     def __init__(self):
@@ -9,11 +11,24 @@ class mind_map:
         
         self.printable_map = []
         
+    def player_enter(self, y, x):
         
+        var = self.map_list[y] 
+        ver =var[x]   
+        var[x] = var[x] + 16
+        # print(ver)
+        # print(var[x])
         
+    def player_exit(self, y, x):
+        var = self.map_list[y] 
+        ver =var[x]   
+        var[x] = var[x] - 16
+        # print(ver)
+        # print(var[x])
             
     def print_map(self):
         print("")
+        self.printable_map = []
         for row in self.map_list:
             n_row = []
             for room in row:
@@ -29,97 +44,38 @@ class mind_map:
             top = ""
             mid = ""
             bot = ""
-            
+            ext = ""
             for part in row:
                 top = top + part[0]
                 mid = mid + part[1]
                 bot = bot + part[2]
+                ext = ext + part[3]
                 
-            p_map = p_map + top + "\n" + mid + "\n" + bot + "\n"
+            p_map = p_map + top + "\n" + mid + "\n" + bot + "\n" + ext + "\n"
             
         print(p_map)
 
-                               
-
-    # def make_map(self):
-    #     print("word")
-        
-    #     map = []
-    #     print(map)
-        
-    #     for i in range(5):
-    #         row = []
-    #         for j in range(6):
-    #             row.append(" 1 ")   
-    #         map.append(row)
-        
-    #     print(map)
-
-    def display_map(self):
-        # print("")
-        
-        for i, row in enumerate(self.map_list):
-            print("")
-            for j, item in enumerate(row):
-                print(item  ,end="")
-
-def all_characters():
-    print()
-    
-    for i in range(20):
-        # if i%10 == 9:
-            
-        #     print("", end="")
-            
-            
-        print(i , " : ", chr(i), " ")
-
-def all_characters_2():
-    print()
-    # -*- coding: utf-8 -*- 
-    for i in range(0xB3, 0xDA):
-        print(chr(i).decode('cp437'))
-
-    # without decoding (see comment by J.F.Sebastian)
-    print(''.join(map(chr, range(0xb3, 0xda))))
-
-
-def test():
-    chars = {
-    'a': '┌',
-    'b': '┐',
-    'c': '┘',
-    'd': '└',
-    'e': '─',
-    'f': '│',
-    'g': '┴',
-    'h': '├',
-    'i': '┬',
-    'j': '┤',
-    'k': '╷',
-    'l': '┼'
-    }
-    print("")
-    print(chars)
 
 def make_box(num):
     
-    top_1 = "┌─────┐"
-    top_2 = "┌──┴──┐"
+    top_1 = " ┌─────┐ "
+    top_2 = " ┌──┴──┐ "
     
-    mid_1 = "│     │" 
-    mid_l = "┤     │"
-    mid_r = "│     ├"
-    mid_2 = "┤     ├"
+    mid_1 = " │     │ " 
+    mid_l = "─┤     │ "
+    mid_r = " │     ├─"
+    mid_2 = "─┤     ├─"
     
-    p_m_1 = "│  X  │" 
-    p_m_l = "┤  X  │"
-    p_m_r = "│  X  ├"
-    p_m_2 = "┤  X  ├"
+    p_m_1 = " │  " + pr_colour("l_green","X") +"  │ " 
+    p_m_l = "─┤  " + pr_colour("l_green","X") +"  │ "
+    p_m_r = " │  " + pr_colour("l_green","X") +"  ├─"
+    p_m_2 = "─┤  " + pr_colour("l_green","X") +"  ├─"
     
-    bot_1 = "└─────┘"
-    bot_2 = "└──┬──┘"
-    empty = "       "
+    bot_1 = " └─────┘ "
+    bot_2 = " └──┬──┘ "
+    
+    extra = "    │    "
+    empty = "         "
     
     box_code = str(bin(num))
     box_code = box_code[2 : : ]
@@ -136,7 +92,7 @@ def make_box(num):
     
     box =  []
     if player == "0" and bottom == "0" and top == "0" and left == "0" and right == "0":
-        box = [empty, empty, empty]
+        box = [empty, empty, empty, empty]
     else:
         
         # Box Top
@@ -167,61 +123,25 @@ def make_box(num):
         
         if bottom == "0":
             box.append(bot_1)
+            box.append(empty)
         else:
             box.append(bot_2)
+            box.append(extra)
             
     # for parts in box:    
     #     print(parts)
     return box
 
-def make_boxes():
-    top_1 = "┌─────┐"
-    top_2 = "┌──┴──┐"
-    
-    mid_1 = "│     │" 
-    mid_l = "┤     │"
-    mid_r = "│     ├"
-    mid_2 = "┤     ├"
-    
-    p_m_1 = "│  X  │" 
-    p_m_l = "┤  X  │"
-    p_m_r = "│  X  ├"
-    p_m_2 = "┤  X  ├"
-    
-    bot_1 = "└─────┘"
-    bot_2 = "└──┬──┘"
-    empty = "       "
-    
-    
-    row1 = [top_1, top_1, top_1]
-    row2 = [mid_r, mid_2, mid_l]
-    row3 = [bot_1, bot_2, bot_2]
-    row4 = [top_1, top_2, top_2]
-    row5 = [mid_1, mid_r, mid_l]
-    row6 = [bot_2, bot_2, bot_1]
-    row7 = [top_2, top_2, top_1]
-    row8 = [mid_r, mid_2, mid_l]
-    row9 = [bot_1, bot_1, bot_1]    
-    
-    grid = [row1, row2, row3, row4, row5, row6, row7, row8, row9 ]
-    
-    for row in grid:
-        print()
-        for part in row:
-            print(part, end="")
-
 if __name__ =="__main__":
     
     myMap = mind_map()
-    # myMap.display_map()
-    
-    # all_characters_2()
-    
-    # make_boxes()
-    
-    # for i in range(0,31):
-    #     print("Box {}: ".format(i))
-    #     make_box(i)
-    #     print()
-    
     myMap.print_map()
+    myMap.player_enter(2,1)
+    clear_screen()
+    myMap.print_map()
+    pause()
+    myMap.player_exit(2,1)
+    myMap.player_enter(2,2)
+    clear_screen()
+    myMap.print_map()
+    pause()
