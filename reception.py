10f73e408add19035928d9749fd5daec2f30677b
JoshuaMcroberts <josh.mcr@hotmail.co.uk>
2021-12-20 21:26:50 +0000

    added game over and sec guard tracking

--- reception.py
+++ reception.py
@@ -4,50 +4,61 @@ from game import N_game
 from security_office import *
 
 def recep(game = N_game()):
-   
-    
     loop = True
     while loop:
-        game.game_map.pre = game.game_map.player_enter((4,3),game.game_map.pre)   
-        clear_screen()
-        print_tab(pr_colour("l_blue","-- RECEPTION --")+"\n")
-        print_tab("The foyer of the building stretches out in a large oval with the narrow ends on the left and right.")
-        print_tab("A semi-circler, tall " + pr_colour("l_blue","Reception Desk") + " is prominently placed against the back wall of the room with a ")
-        print_tab("large double " + pr_colour("l_blue","Glass Door") + " to its left which leads further into the building.")
-        print_tab("On the left side of the room there is another door with the name plate on it saying, " + pr_colour("l_blue","Security Office") + ". ")
-        print_tab("As you look to the right of the room, you see a " + pr_colour("l_blue","Lift") + " door and a " + pr_colour("l_blue","Waiting Area") + " which is ")
-        print_tab("nestled back up against the glass front windows. ")
-        var = san_input()
-        
-       
-        # Navigation IF  
-        if var  == "receptiondesk":
-            recep_desk(game)
-            
-        elif var == "glassdoor":
-            glass_door(game)
-            
-        elif var == "securityoffice":
-            sec_office(game)
-            
-        elif var == "lift":
-            lift(game)
-            
-        elif var == "waitingarea":
-            waiting_area(game)
-            
-        elif var == "exit":
-            loop = False
-            
-        elif var == "map":
+        if(game.game_over == False):
+            if (game.sec_gar == False and game.game_map.pre == (4,2)):
+                game.sec_gar = True
+                clear_screen()
+                print_tab(pr_colour("l_blue","-- LEAVING THE SECURITY OFFICE --")+"\n")
+                print_tab("As you exit the security office you look through the Glass door down the hallway. You see the Security Guard")
+                print_tab("Just rounding the corner into the main corridor on his return journey. That was good timing, another minute")
+                print_tab("and he would have caught you. Step into the centre of the reception as he enters and quickly moves passed you")
+                print_tab("back to the Security Office")
+                pause()
+                
+            game.game_map.pre = game.game_map.player_enter((4,3),game.game_map.pre)   
             clear_screen()
-            game.game_map.print_map()
-            pause()
+            print_tab(pr_colour("l_blue","-- RECEPTION --")+"\n")
+            print_tab("The foyer of the building stretches out in a large oval with the narrow ends on the left and right.")
+            print_tab("A semi-circler, tall " + pr_colour("l_blue","Reception Desk") + " is prominently placed against the back wall of the room with a ")
+            print_tab("large double " + pr_colour("l_blue","Glass Door") + " to its left which leads further into the building.")
+            print_tab("On the left side of the room there is another door with the name plate on it saying, " + pr_colour("l_blue","Security Office") + ". ")
+            print_tab("As you look to the right of the room, you see a " + pr_colour("l_blue","Lift") + " door and a " + pr_colour("l_blue","Waiting Area") + " which is ")
+            print_tab("nestled back up against the glass front windows. ")
+            var = san_input()
+            
         
+            # Navigation IF  
+            if var  == "receptiondesk":
+                recep_desk(game)
+                
+            elif var == "glassdoor":
+                glass_door(game)
+                
+            elif var == "securityoffice":
+                sec_office(game)
+                
+            elif var == "lift":
+                lift(game)
+                
+            elif var == "waitingarea":
+                waiting_area(game)
+                
+            elif var == "exit":
+                loop = False
+                
+            elif var == "map":
+                clear_screen()
+                game.game_map.print_map()
+                pause()
+            
+            else:
+                print("")
+                print_tab("Incorrect entry try again")
+                pause()
         else:
-            print("")
-            print_tab("Incorrect entry try again")
-            pause()
+            loop = False
 
 def recep_desk(game = N_game()):
     clear_screen()
@@ -105,77 +116,97 @@ def lift(game = N_game()):
     
 def waiting_area(game = N_game()):
     # 2nd Prioity - gaurd leave office
-    wait = 0
+    wait = game.wait
     loop = True
     while loop:
-        clear_screen()
-        print_tab(pr_colour("l_blue","-- WAITING AREA --") + "\n")
-        print_tab("The waiting area consists of three tan leather sofas set in a U shape, with the open")
-        print_tab("end facing the main entrance. This gives each sofa a varying view, from the reception")
-        print_tab("to the those entering through the main door and the final facing out the window to ")
-        print_tab("the carpark area outside. There are a few magazines laid out on a short coffee table")
-        print_tab("in the middle of the U shape to help pass the " + pr_colour("l_blue","Wait") + ".")
-        var = san_input()
-
-        if var == "wait":
-            if wait < 4:
-                clear_screen()
-                print_tab("<character> rests in the waiting area for half an hour. Nothing happens.")
-                wait += 1
-                pause()
-                # notepad option change wait < 4 to 3
-            elif wait < 5:
-                clear_screen()
-                print_tab("<character> rests in the waiting area for half an hour. The Receptionist gets up, scans")
-                print_tab("her pass and leaves through the glass door, only to return 5 mins later with a cup")
-                print_tab("of coffee and a small piece of fruit.")
-                wait += 1
-                pause()
+        if(game.game_over == False):
+            clear_screen()
+            print_tab(pr_colour("l_blue","-- WAITING AREA --") + "\n")
+            print_tab("The waiting area consists of three tan leather sofas set in a U shape, with the open")
+            print_tab("end facing the main entrance. This gives each sofa a varying view, from the reception")
+            print_tab("to the those entering through the main door and the final facing out the window to ")
+            print_tab("the carpark area outside. There are a few magazines laid out on a short coffee table")
+            print_tab("in the middle of the U shape to help pass the " + pr_colour("l_blue","Wait") + ".")
+            var = san_input()
+            char_name = game.pc.character_name
+            if var == "wait":
+                if wait < 1:
+                    clear_screen()
+                    print_tab(char_name + " rests in the waiting area for half an hour. Nothing happens.")
+                    wait += 1
+                    pause()
+                    
+                elif wait < 2:
+                    clear_screen()
+                    print_tab(char_name + " rests in the waiting area for half an hour. As you are about to get up the Security")
+                    print_tab("Guard leaves his office, scans his card at the glass door and passes through.") 
+                    print_tab("Best take your chance to check out the Security office!")
+                    wait += 1
+                    game.sec_gar = False
+                    pause()
                 
-            elif wait < 6:
-                clear_screen()
-                print_tab("<character> rests in the waiting area for half an hour. A delivery man enters the reception and drags a")
-                print_tab("sack truck across the floor which appears to have one flat wheel. The security guard emerges from")
-                print_tab("his office and escorts the delivery man through the glass door and then back out again after another")
-                print_tab("10 minutes has passed.")
-                wait += 1
-                pause()
+                elif wait < 3:
+                    clear_screen()
+                    print_tab(char_name + " rests in the waiting area for half an hour. Nothing happens.")
+                    wait += 1
+                    pause()
                 
-            elif wait < 8:
-                clear_screen()
-                print_tab("<character> grows tired of waiting. Another half hour passes.")
-                wait += 1
-                pause()
+                    # notepad option change wait < 4 to 3
+                elif wait < 5:
+                    clear_screen()
+                    print_tab(char_name + " rests in the waiting area for half an hour. The Receptionist gets up, scans")
+                    print_tab("her pass and leaves through the glass door, only to return 5 mins later with a cup")
+                    print_tab("of coffee and a small piece of fruit.")
+                    wait += 1
+                    pause()
+                    
+                elif wait < 6:
+                    clear_screen()
+                    print_tab(char_name + " rests in the waiting area for half an hour. A delivery man enters the reception and drags a")
+                    print_tab("sack truck across the floor which appears to have one flat wheel. The security guard emerges from")
+                    print_tab("his office and escorts the delivery man through the glass door and then back out again after another")
+                    print_tab("10 minutes has passed.")
+                    wait += 1
+                    pause()
+                    
+                elif wait < 8:
+                    clear_screen()
+                    print_tab(char_name + " grows tired of waiting. Another half hour passes.")
+                    wait += 1
+                    pause()
+                    
+                elif wait < 9:
+                    clear_screen()
+                    print_tab(char_name + " rests in the waiting area for half an hour. Closing time approaches.")
+                    wait += 1
+                    pause()
+                    
+                else:
+                    clear_screen()
+                    print_tab(char_name + " rests in the waiting area. The Security Guard emerges from his office and escorts <character>")
+                    print_tab("out of the building as it is closing time. ")
+                    print_tab("Game Over")
+                    game.game_over = True
+                    pause()
                 
-            elif wait < 9:
+                game.wait = wait    
+                
+            elif var == "back":
+                # not wait
+                print_tab("")    
+                loop = False
+                
+            elif var == "map":
                 clear_screen()
-                print_tab("<character> rests in the waiting area for half an hour. Closing time approaches.")
-                wait += 1
-                pause()
+                game.game_map.print_map()
+                pause()    
                 
             else:
                 clear_screen()
-                print_tab("<character> rests in the waiting area. The Security Guard emerges from his office and escorts <character>")
-                print_tab("out of the building as it is closing time. ")
-                print_tab("Game Over")
-                # End game here
+                print_tab("Try typing 'Back'")
                 pause()
-                
-        elif var == "back":
-            # not wait
-            print_tab("")    
-            loop = False
-            
-        elif var == "map":
-            clear_screen()
-            game.game_map.print_map()
-            pause()    
-            
         else:
-            clear_screen()
-            print_tab("Try typing 'Back'")
-            pause()
-            
+            loop = False
     
 
 if __name__ == "__main__":
