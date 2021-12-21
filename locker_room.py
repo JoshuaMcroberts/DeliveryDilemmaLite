eb0059b735f4e229916e272a9430b0674bda34aa
JoshuaMcroberts <josh.mcr@hotmail.co.uk>
2021-12-21 21:28:10 +0000

    Updated text, ID card in locker 21 added

--- locker_room.py
+++ locker_room.py
@@ -12,7 +12,7 @@ def locker_room(game = N_game()):
         print_tab(pr_colour("l_blue","-- LOCKER ROOM --")+"\n")
         print_tab("lockers")
         print_tab("The room is narrow and bordering three walls are a collection of storage lockers. ")
-        print_tab("Ten full size lockers are against the right-hand wall with a matching Ten opposite ")
+        print_tab("Ten full size " + pr_colour("l_blue","Lockers") + " are against the right-hand wall with a matching Ten opposite ")
         print_tab("them against the left-hand wall. The wall towards the back of the room is roughly ")
         print_tab("half the size of the other two walls and therefore has two rows of five half-sized ")
         print_tab("lockers which had been stacked to give in total an additional Ten lockers. ")
@@ -29,14 +29,17 @@ def locker_room(game = N_game()):
         elif var == "back":
             loop = False                # Back value
             
-        elif var == "<playername>":
-            print("inventory")
-            # placeholder function
+        elif var == san_text(game.pc.char_name):
+            clear_screen()
+            game.pc.display_inventory()
+            pause()
             
         elif var == "map":
             clear_screen()
             game.game_map.print_map()
+          
             pause()
+            
         elif var == "hint":
             clear_screen()
             # custom help text for room
@@ -44,6 +47,7 @@ def locker_room(game = N_game()):
         else:
             print("")
             print_tab("Incorrect entry try again")
+            
             pause()
 
 
@@ -55,22 +59,21 @@ def lockers(game = N_game()):
         clear_screen()
         # update text
         print_tab(pr_colour("l_blue","-- LOCKERS --") + "\n")
-        print_tab("locker 1 - locker 30")
-        print_tab("Enter the locker number you wish to search by typing: 'Locker <NUMBER>'.")
+        print_tab("Enter the locker number you wish to search:" )
         var, num = item_input()
-        
+              
         if var == "locker" and num > 0 and num < 31:
         
             if num < 14:
                 clear_screen()
                 print_tab(pr_colour("l_blue","-- LOCKER " + str(num) + " --") + "\n")
-                print_tab("You open " + pr_colour("l_blue","locker " + str(num)) + " and find that it is empty.")
+                print_tab("You open " + pr_colour("l_blue","Locker " + str(num)) + " and find that it is empty.")
                 pause()
                            
             elif num < 21:
                 clear_screen()
                 print_tab(pr_colour("l_blue","-- LOCKER " + str(num) + " --") + "\n")
-                print_tab("You go to open " + pr_colour("l_blue","'locker ' + num") + " but it appears to be locked.")
+                print_tab("You go to open " + pr_colour("l_blue","Locker " + str(num)) + " but it appears to be locked.")
                 pause()
             
             elif num == 21:
@@ -79,25 +82,33 @@ def lockers(game = N_game()):
             elif num < 26:
                 clear_screen()
                 print_tab(pr_colour("l_blue","-- LOCKER " + str(num) + " --") + "\n")
-                print_tab("As you open " + pr_colour("l_blue","'locker ' + num") + " you hear someone else enter the room. You pretend to be retrieving ")
-                print_tab("something from your non-existent bag in the empty locker. The person passes behind you and goes ")
-                print_tab("further down your row. Out of the corner of your eye you see them take their pass from around ")
-                print_tab("their neck and put it inside the locker. They then take out a lunch box and locking their locker behind ")
-                print_tab("them. As they exit the room they slip their locker key into the pocket of their worn work coat. ")
-                # Progression
+                
+                if game.get_key == False:
+                    print_tab("As you open " + pr_colour("l_blue","Locker " + str(num)) + " you hear someone else enter the room. You pretend to be retrieving ")
+                    print_tab("something from your non-existent bag in the empty locker. The person passes behind you and goes ")
+                    print_tab("further down your row. Out of the corner of your eye you see them take their pass from around ")
+                    print_tab("their neck and put it inside the locker. They then take out a lunch box and locking their locker behind ")
+                    print_tab("them. As they exit the room they slip their locker key into the pocket of their worn work coat. ")
+                    game.get_key = True
+                else:
+                    print_tab("You open " + pr_colour("l_blue","Locker " + str(num)) + " and find that it is empty.")
                 pause()     
                 
             elif num < 31:
                 clear_screen()
-                print_tab("below 31")
+                print_tab(pr_colour("l_blue","-- LOCKER " + str(num) + " --") + "\n")
+                print_tab("You go to open " + pr_colour("l_blue","Locker " + str(num)) + " but it appears to be locked.")
                 pause()
+                           
                 
         elif var == "back":
             loop = False
             # loop break value
             
-        elif var == "<playername>":
-            print("inventory")
+        elif var == san_text(game.pc.char_name):
+            clear_screen()
+            game.pc.display_inventory()
+            pause()
             
         elif var == "map":
             clear_screen()
@@ -110,196 +121,37 @@ def lockers(game = N_game()):
             
         else:
             print("")
-            print_tab("Incorrect entry try again")
+            print_tab("Incorrect entry try again, use the format: (Locker NUMBER)")
             pause()
 
 def locker_21(game = N_game()):
     
     clear_screen()
     print_tab(pr_colour("l_blue","-- LOCKER 21 --") + "\n")
+    bol = game.pc.check_inventory("Locker 21 - Key")
     
-    if 1 == 2 and 3 == 3:
+    if bol and game.locker_21_empty == False:
         print_tab("You slide the key for locker 21 into the lock and open it. Inside the locker is a black backpack which is ")
-        print_tab("open and a security pass with the words ‘ Marvin Bleak – Warehouse Opts ’ on it. You take the pass ")
+        print_tab("open and a security pass with the words 'Marvin Bleak - Warehouse Opts' on it. You take the pass ")
         print_tab("and relock the locker. ")
-        inventory # add ware house id pass to inventory
+        game.locker_21_empty = True
+        game.pc.add_inventory("Warehouse - ID Card")
+        
    
-    elif 1 == 2 and 3 == 4:
+    elif bol and game.locker_21_empty:
         print_tab("You slide the key for locker 21 into the lock and open it. Inside the locker is a black backpack which is ")
         print_tab("open and seems to have had a box removed from it. Nothing useful here, you close and relock the locker.")
     
     else:
-        print_tab("You go to open " + pr_colour("l_blue","'locker ' + num") + " but it appears to be locked.")
+        print_tab("You go to open " + pr_colour("l_blue","Locker 21") + " but it appears to be locked.")
     ## Locked
     
     pause()
     
-      
-
-def hall_3(game = N_game()):
-    
-    loop = True
-    while loop:
-        game.game_map.pre = game.game_map.player_enter((1,3),game.game_map.pre)   
-        clear_screen()
-        # update text
-        print_tab(pr_colour("l_blue","-- HALL WAY (3) --") + "\n")
-        print_tab("supplycart forward back.")
-        print_tab("It looks like a " + pr_colour("l_blue", "Card Reader") + ". You can see the hallway beyond with a few doors ")
-        print_tab("leading off to either side.")
-        var = san_input()
-        
-        
-        if var == "supplycart":
-            supply_cart(game)
-        
-        elif var == "forward":
-            hall_4(game)
-             
-        elif var == "back":
-            loop = False
-            # loop break value
-        
-        elif var == "<playername>":
-            print("inventory")
-        
-        elif var == "map":
-            clear_screen()
-            game.game_map.print_map()
-            pause()
-        
-        elif var == "hint":
-            clear_screen()
-            # custom help text for room
-        
-        else:
-            print("")
-            print_tab("Incorrect entry try again")
-            pause()
-
-
-    
-def supply_cart(game = N_game()):
-    clear_screen()
-    print_tab(pr_colour("l_blue","-- CLEANING SUPPLY CART --") + "\n")
-    print_tab("You walk over to the lift to take a closer look. There is a number pad and")
-    print_tab("a card reader next to the door however above them is a seemingly freshly")
-    print_tab("printed piece of paper taped to the wall saying “Out of Order”")
-    pause()
-
-def hall_4(game = N_game()):
-    
-    loop = True
-    while loop:
-        game.game_map.pre = game.game_map.player_enter((0,3),game.game_map.pre)   
-        clear_screen()
-        # update text
-        print_tab(pr_colour("l_blue","-- HALL WAY (4) --") + "\n")
-        print_tab("stairs right back.")
-        print_tab("It looks like a " + pr_colour("l_blue", "Card Reader") + ". You can see the hallway beyond with a few doors ")
-        print_tab("leading off to either side.")
-        var = san_input()
-        
-        
-        if var == "stairs":
-            stairs(game)
-        
-        elif var == "right":
-            hall_6(game)
-             
-        elif var == "back":
-            loop = False
-            # loop break value
-        
-        elif var == "<playername>":
-            print("inventory")
-        
-        elif var == "map":
-            clear_screen()
-            game.game_map.print_map()
-            pause()
-        
-        elif var == "hint":
-            clear_screen()
-            # custom help text for room
-        
-        else:
-            print("")
-            print_tab("Incorrect entry try again")
-            pause()
-    
-
-def stairs(game = N_game()):
-    # 2nd Prioity - gaurd leave office
-    clear_screen()
-    print_tab(pr_colour("l_blue","-- STAIR WELL --") + "\n")
-    print_tab("You walk over to the lift to take a closer look. There is a number pad and")
-    print_tab("a card reader next to the door however above them is a seemingly freshly")
-    print_tab("printed piece of paper taped to the wall saying “Out of Order”")
-    pause()
-
-       
-def hall_6(game = N_game()):
-    
-    loop = True
-    while loop:
-        game.game_map.pre = game.game_map.player_enter((0,4),game.game_map.pre)   
-        clear_screen()
-        # update text
-        print_tab(pr_colour("l_blue","-- HALL WAY (6) --") + "\n")
-        print_tab("toilets fireexit back.")
-        print_tab("It looks like a " + pr_colour("l_blue", "Card Reader") + ". You can see the hallway beyond with a few doors ")
-        print_tab("leading off to either side.")
-        var = san_input()
-        
-        
-        if var == "toilets":
-            toilets(game)
-        
-        elif var == "fireexit":
-            fire_exit(game)
-             
-        elif var == "back":
-            loop = False
-            # loop break value
-        
-        elif var == "<playername>":
-            print("inventory")
-        
-        elif var == "map":
-            clear_screen()
-            game.game_map.print_map()
-            pause()
-        
-        elif var == "hint":
-            clear_screen()
-            # custom help text for room
-        
-        else:
-            print("")
-            print_tab("Incorrect entry try again")
-            pause()
-    
-def toilets(game = N_game()):
-    # 2nd Prioity - gaurd leave office
-    clear_screen()
-    print_tab(pr_colour("l_blue","-- TOILETS --") + "\n")
-    print_tab("You walk over to the lift to take a closer look. There is a number pad and")
-    print_tab("a card reader next to the door however above them is a seemingly freshly")
-    print_tab("printed piece of paper taped to the wall saying “Out of Order”")
-    pause()
-    
-def fire_exit(game = N_game()):
-    # 2nd Prioity - gaurd leave office
-    clear_screen()
-    print_tab(pr_colour("l_blue","-- FIRE EXIT --") + "\n")
-    print_tab("You walk over to the lift to take a closer look. There is a number pad and")
-    print_tab("a card reader next to the door however above them is a seemingly freshly")
-    print_tab("printed piece of paper taped to the wall saying “Out of Order”")
-    pause()
 
 
 if __name__ == "__main__":
     game = N_game()
+    game.pc.inventory = ["Locker 21 - Key"]
     locker_room(game)
 
