104830bf47a24167e63da12f541d87e2a3015e4b
JoshuaMcroberts <64220575+JoshuaMcroberts@users.noreply.github.com>
2021-12-21 09:31:54 +0000

    Text Update for main halls

--- halls.py
+++ halls.py
@@ -14,11 +14,9 @@ def hall_1(game = N_game()):
         # update text
         print_tab(pr_colour("l_blue","-- HALL WAY (1) --")+"\n")
         print_tab("forward adminoffice back")
-        print_tab("A semi-circler, tall " + pr_colour("l_blue","Reception Desk") + " is prominently placed against the back wall of the room with a ")
-        print_tab("large double " + pr_colour("l_blue","Glass Door") + " to its left which leads further into the building.")
-        print_tab("On the left side of the room there is another door with the name plate on it saying, " + pr_colour("l_blue","Security Office") + ". ")
-        print_tab("As you look to the right of the room, you see a " + pr_colour("l_blue","Lift") + " door and a " + pr_colour("l_blue","Waiting Area") + " which is ")
-        print_tab("nestled back up against the glass front windows.")
+        print_tab("The walls of this hallway are decorated with commemorative plaques with the names of the employ")
+        print_tab("of the year engraved on them. There is a door on the right with the words " + pr_colour("l_blue","Administration Office"))
+        print_tab("on it. The corridor also continues " + pr_colour("l_blue","forward") +".")
         var = san_input()
         
        
@@ -32,7 +30,7 @@ def hall_1(game = N_game()):
         elif var == "back":
             loop = False                # Back value
             
-        elif var == "<playername>":
+        elif var == game.pc.character_name:
             print("inventory")
             # placeholder function
             
@@ -52,9 +50,7 @@ def hall_1(game = N_game()):
 def admin_office(game = N_game()):
     clear_screen()
     print_tab(pr_colour("l_blue","-- ADMIN OFFICE --") + "\n")
-    print_tab("You walk over to the lift to take a closer look. There is a number pad and")
-    print_tab("a card reader next to the door however above them is a seemingly freshly")
-    print_tab("printed piece of paper taped to the wall saying “Out of Order”")
+    print_tab("You try the Admin Office door and it is locked.")
     pause()
        
 def hall_2(game = N_game()):
@@ -66,8 +62,8 @@ def hall_2(game = N_game()):
         # update text
         print_tab(pr_colour("l_blue","-- HALL WAY (2) --") + "\n")
         print_tab("left forward back")
-        print_tab("It looks like a " + pr_colour("l_blue", "Card Reader") + ". You can see the hallway beyond with a few doors ")
-        print_tab("leading off to either side.")
+        print_tab("The hallway continues on until another corridor branches off from it to the " + pr_colour("l_blue", "left") + ".  The corridor also ")
+        print_tab("continues " + pr_colour("l_blue", "forward") + " with pictures on the wall, of the different logos " + game.courier + " had used in the past.")
         var = san_input()
         
         
@@ -105,11 +101,14 @@ def hall_5(game = N_game()):
     while loop:
         game.game_map.pre = game.game_map.player_enter((2,2),game.game_map.pre)   
         clear_screen()
-        # update text
+        
         print_tab(pr_colour("l_blue","-- HALL WAY (5) --") + "\n")
         print_tab("canteen lockerroom door back.")
-        print_tab("It looks like a " + pr_colour("l_blue", "Card Reader") + ". You can see the hallway beyond with a few doors ")
-        print_tab("leading off to either side.")
+        print_tab("The corridor ends with 3 doors, the one directly ahead has nothing on it however above it on the ")
+        print_tab("brick work, in spray painted stencil font, is the word " + pr_colour("l_blue","Warehouse") + ". The Door to the left has a metal ")
+        print_tab("plate screwed to it with the words " + pr_colour("l_blue","Locker Room") + " etched into it. The door on the right has on it a ")
+        print_tab("plate that matches the opposing Locker Room door with the one change that it displays the word ")
+        print_tab(pr_colour("l_blue","Canteen") + " on it.")
         var = san_input()
         
         if var == "canteen":
@@ -143,7 +142,7 @@ def hall_5(game = N_game()):
             pause()
 
 def hall_3(game = N_game()):
-    
+    # Not Complete
     loop = True
     while loop:
         game.game_map.pre = game.game_map.player_enter((1,3),game.game_map.pre)   
@@ -186,6 +185,7 @@ def hall_3(game = N_game()):
 
     
 def supply_cart(game = N_game()):
+    # Not Complete
     clear_screen()
     print_tab(pr_colour("l_blue","-- CLEANING SUPPLY CART --") + "\n")
     print_tab("You walk over to the lift to take a closer look. There is a number pad and")
@@ -194,7 +194,7 @@ def supply_cart(game = N_game()):
     pause()
 
 def hall_4(game = N_game()):
-    
+    # Not Complete
     loop = True
     while loop:
         game.game_map.pre = game.game_map.player_enter((0,3),game.game_map.pre)   
@@ -236,7 +236,7 @@ def hall_4(game = N_game()):
     
 
 def stairs(game = N_game()):
-    # 2nd Prioity - gaurd leave office
+    # Not Complete
     clear_screen()
     print_tab(pr_colour("l_blue","-- STAIR WELL --") + "\n")
     print_tab("You walk over to the lift to take a closer look. There is a number pad and")
@@ -246,7 +246,7 @@ def stairs(game = N_game()):
 
        
 def hall_6(game = N_game()):
-    
+    # Not Complete
     loop = True
     while loop:
         game.game_map.pre = game.game_map.player_enter((0,4),game.game_map.pre)   
@@ -287,7 +287,7 @@ def hall_6(game = N_game()):
             pause()
     
 def toilets(game = N_game()):
-    # 2nd Prioity - gaurd leave office
+    # Not Complete
     clear_screen()
     print_tab(pr_colour("l_blue","-- TOILETS --") + "\n")
     print_tab("You walk over to the lift to take a closer look. There is a number pad and")
@@ -296,7 +296,7 @@ def toilets(game = N_game()):
     pause()
     
 def fire_exit(game = N_game()):
-    # 2nd Prioity - gaurd leave office
+    # Not Complete
     clear_screen()
     print_tab(pr_colour("l_blue","-- FIRE EXIT --") + "\n")
     print_tab("You walk over to the lift to take a closer look. There is a number pad and")
